e9a64e16f1aefc28ca758d48ac24f6d3899228c2
Akhil Anil <akhilanil95@gmail.com>
2016-09-27 23:02:09 +0530

    Update Connect_db.java

--- src/connection/Connect_db.java
+++ src/connection/Connect_db.java
@@ -3,7 +3,7 @@ import java.sql.*;
 
 public class Connect_db{
 	public Connection connect() throws Exception{
-		Connection blood_db=DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Master;user=sa;password=lostandfounD");
+		Connection blood_db=DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Master;user=sa;password=password");
 		return blood_db;
 	}
 }
